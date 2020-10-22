68d55e6b40d2b9912c644f26f0b9b983ceb4b528
wilana <wsmatthe@lakeheadu.ca>
2020-10-22 19:08:10 -0400

    Started creating a bar chart view to open with

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-import Utilities.CSVUtility;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
 
-        File plantsCSV = new File("src/plants.csv");
+        /*File plantsCSV = new File("src/plants.csv");
         try {
             CSVUtility.convertCSVtoSQL(plantsCSV, "plantsSQL.sql");
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Views/BarChartView.fxml"));
+        Scene scene = new Scene(root);
+        // Add title and show
+        stage.setTitle("Create Student");
+        stage.setScene(scene);
+        stage.show();
     }
 }
